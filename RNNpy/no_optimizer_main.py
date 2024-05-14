e668f45adbbd52be513240860fd993a90ab740bb
FerDovah <fernando.revillas.gonzalez@hotmail.es>
2024-05-14 15:23:11 +0200

    Create, train and test model (without optimizer)

--- RNNpy/no_optimizer_main.py
+++ RNNpy/no_optimizer_main.py
@@ -1,6 +1,6 @@
 from rnn import objective_function
 
-parameters = [31, 13, 5, 0.00502, 1] # [lstm_size1, lstm_size2, num_epochs, learning_rate, batch_size]
+parameters = [15, 10, 2, 0.00502, 1] # [lstm_size1, lstm_size2, num_epochs, learning_rate, batch_size]
 
 print(f"Parameters of the model: {parameters}")
 
